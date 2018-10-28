a081b845ab44b67861d7891da6bf5704fecd5361
Jovana Andrejevic <jandrejevic@g.harvard.edu>
2018-10-27 22:57:07 -0400

    Added more operators and elementary functions; elementary functions not yet migrated to their own module.

--- autodiff/autodiff/autodiff.py
+++ autodiff/autodiff/autodiff.py
@@ -44,6 +44,15 @@ class Operation:
     def __radd__(self, other):
         return self + other
 
+    def __sub__(self, other):
+        if isinstance(other, Operation):
+            return Subtraction(self, other)
+        else:
+            return Subtraction(self, Constant(other))
+
+    def __rsub__(self, other):
+        return Constant(other) - self
+
     def __mul__(self, other):
         if isinstance(other, Operation):
             return Multiplication(self, other)
@@ -52,6 +61,15 @@ class Operation:
 
     def __rmul__(self, other):
         return self * other
+
+    def __truediv__(self, other):
+        if isinstance(other, Operation):
+            return Division(self, other)
+        else:
+            return Division(self, Constant(other))
+
+    def __rtruediv__(self, other):
+        return Constant(other) / self
 
     def __pow__(self, power, modulo=None):
         if isinstance(power, Operation):
@@ -64,6 +82,12 @@ class Operation:
             return Power(other, self)
         else:
             return Power(Constant(other), self)
+    
+    def __pos__(self):
+        return Pos(self)
+
+    def __neg__(self):
+        return Neg(self)
 
     def evaluate(self):
         raise NotImplementedError
@@ -103,7 +127,6 @@ class Constant(Operation):
     def der(self, op):
         return 0
 
-
 class Addition(Operation):
     def __init__(self, op_1, op_2, ID=None):
         super().__init__(ID=ID)
@@ -120,6 +143,21 @@ class Addition(Operation):
     def evaluate(self):
         self._value = self.op_1.value + self.op_2.value
 
+class Subtraction(Operation):
+    def __init__(self, op_1, op_2, ID=None):
+        super().__init__(ID=ID)
+        self.op_1 = op_1
+        self.op_2 = op_2
+
+    def der(self, op):
+        self.evaluate()
+        derivative = 0
+        derivative += self.op_1.der(op)
+        derivative -= self.op_2.der(op)
+        return derivative
+
+    def evaluate(self):
+        self._value = self.op_1.value - self.op_2.value
 
 class Multiplication(Operation):
     def __init__(self, op_1, op_2, ID=None):
@@ -137,6 +175,21 @@ class Multiplication(Operation):
     def evaluate(self):
         self._value = self.op_1.value * self.op_2.value
 
+class Division(Operation):
+    def __init__(self, op_1, op_2, ID=None):
+        super().__init__(ID=ID)
+        self.op_1 = op_1
+        self.op_2 = op_2
+
+    def der(self, op):
+        self.evaluate()
+        derivative = 0
+        derivative += (self.op_2.value * self.op_1.der(op))/(self.op_2.value)**2
+        derivative -= (self.op_1.value * self.op_2.der(op))/(self.op_2.value)**2
+        return derivative
+
+    def evaluate(self):
+        self._value = self.op_1.value / self.op_2.value
 
 class Power(Operation):
     def __init__(self, op_1, op_2, ID=None):
@@ -156,11 +209,34 @@ class Power(Operation):
     def evaluate(self):
         self._value = self.op_1.value**self.op_2.value
 
+class Pos(Operation):
+    def __init__(self, op, ID=None):
+        super().__init__(ID=ID)
+        self.op = op
+
+    def der(self, op):
+        self.evaluate()
+        return self.op.der(op)
+
+    def evaluate(self):
+        self._value = self.op.value
+
+class Neg(Operation):
+    def __init__(self, op, ID=None):
+        super().__init__(ID=ID)
+        self.op = op
+
+    def der(self, op):
+        self.evaluate()
+        return -self.op.der(op)
+
+    def evaluate(self):
+        self._value = -self.op.value
 
 class Exp(Operation):
     def __init__(self, op, ID=None):
         super().__init__(ID=ID)
-        self.op = op
+        self.op = op if isinstance(op, Operation) else Constant(op)
 
     def der(self, op):
         self.evaluate()
@@ -169,18 +245,44 @@ class Exp(Operation):
     def evaluate(self):
         self._value = np.exp(self.op.value)
 
+class Log(Operation):
+    def __init__(self, op, base=Constant(np.e), ID=None):
+        super().__init__(ID=ID)
+        self.op = op if isinstance(op, Operation) else Constant(op)
+        self.base = base if isinstance(base, Operation) else Constant(base)
+
+    def der(self, op):
+        self.evaluate()
+        derivative = 0
+        derivative += (np.log(self.base.value) / self.op.value *
+                self.op.der(op)) / (np.log(self.base.value))**2
+        derivative -= (np.log(self.op.value) / self.base.value *
+                self.base.der(op)) / (np.log(self.base.value))**2
+        return derivative
+
+    def evaluate(self):
+        self._value = np.log(self.op.value) / np.log(self.base.value)
+
+class Sqrt(Operation):
+    def __init__(self, op, ID=None):
+        super().__init__(ID=ID)
+        self.op = op if isinstance(op, Operation) else Constant(op)
+
+    def der(self, op):
+        self.evaluate()
+        return 1./(2*self.value)*self.op.der(op)
+
+    def evaluate(self):
+        self._value = np.sqrt(self.op.value)
 
 class Sin(Operation):
     def __init__(self, op, ID=None):
         super().__init__(ID=ID)
-        self.op = op
+        self.op = op if isinstance(op, Operation) else Constant(op)
 
     def der(self, op):
         self.evaluate()
-        if op == self.op:
-            return np.cos(op.value)
-        else:
-            return np.cos(self.op.value)*self.op.der(op)
+        return np.cos(self.op.value)*self.op.der(op)
 
     def evaluate(self):
         self._value = np.sin(self.op.value)
@@ -189,14 +291,11 @@ class Sin(Operation):
 class Cos(Operation):
     def __init__(self, op, ID=None):
         super().__init__(ID=ID)
-        self.op = op
+        self.op = op if isinstance(op, Operation) else Constant(op)
 
     def der(self, op):
         self.evaluate()
-        if op == self.op:
-            return -np.sin(op.value)
-        else:
-            return -np.sin(self.op.value)*self.op.der(op)
+        return -np.sin(self.op.value)*self.op.der(op)
 
     def evaluate(self):
         self._value = np.cos(self.op.value)
