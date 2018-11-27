03bd87e7b2f6670605c86a8d1278b99d46ed4983
one-for-all <jianan.li@yahoo.com>
2018-11-27 17:14:37 -0500

    add interface module and tests

---
+++ therapy_planner/therapy_planner/tests/test_interface.py
@@ -0,0 +1,18 @@
+from ..interface import read_maps
+import numpy as np
+from numpy.testing import assert_array_equal
+import os
+
+
+# Read file relative to current directory (when using pytest)
+__location__ = os.path.realpath(
+    os.path.join(os.getcwd(), os.path.dirname(__file__)))
+
+
+class TestInterface:
+    def test_read_maps(self):
+        maps = read_maps(os.path.join(__location__, 'test.map'))
+        target_map = np.array([[1, 2, 3],
+                               [4, 5, 6],
+                               [7, 8, 9]])
+        assert_array_equal(maps["target"], target_map)
