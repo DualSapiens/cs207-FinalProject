446d9f658d4cbddc301ebc245019fa9ab1332bed
bayesically <gkotecha@g.harvard.edu>
2018-12-06 14:28:25 -0500

    Added comments to the cost functions

--- therapy_planner/therapy_planner/costfunctions.py
+++ therapy_planner/therapy_planner/costfunctions.py
@@ -8,16 +8,19 @@ import sys
 sys.path.append("../../gradpy")
 from gradpy.math import *
 
-# define some cost functions (can go into their own module)
-def mean_squared_error(y,yo):
-    return sum([(yi-yio)**2 for yi,yio in zip(y,yo)])
+#Define an approximate step function with a tunable sharpness parameter
+def approximate_step_function(x,sharpness=5):
+    return Logistic(x,x_0=0.5,L=2,k=sharpness)
 
 # a penalty function to encourage params >= 0
 def pos_penalty(p,smoothness):
     return sum([Logistic(-pi,k=1./smoothness) for pi in p])
 
-def approximate_step_function(x,sharpness=5):
-    return Logistic(x,x_0=0.5,L=2,k=sharpness)
+# mean squared error for the target parameter
+def mean_squared_error(y,yo):
+    return sum([(yi-yio)**2 for yi,yio in zip(y,yo)])
 
+# Minimax penalty using the approximate step function
 def minmax_penalty(vec):
     return sum(approximate_step_function(x) for x in vec)
+
