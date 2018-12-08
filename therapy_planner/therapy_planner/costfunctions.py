f078cbcc5158cf1c6bf00af6331de456312b8011
Jovana Andrejevic <jandrejevic@g.harvard.edu>
2018-12-08 00:00:57 -0500

    Update default value and format for smoothness.

--- therapy_planner/therapy_planner/costfunctions.py
+++ therapy_planner/therapy_planner/costfunctions.py
@@ -9,18 +9,18 @@ sys.path.append("../../gradpy")
 from gradpy.math import *
 
 #Define an approximate step function with a tunable sharpness parameter
-def approximate_step_function(x,sharpness=5):
-    return Logistic(x,x_0=0.5,L=2,k=sharpness)  #Logistic function f(X) = L/(1+exp(-k(X-x_0)))
+def approximate_step_function(x, smoothness):
+    return Logistic(x,x_0=0.5,L=2,k=1./smoothness)  #Logistic function f(X) = L/(1+exp(-k(X-x_0)))
 
 # a penalty function to encourage params >= 0
-def pos_penalty(p,smoothness):
+def pos_penalty(p, smoothness):
     return sum([Logistic(-pi,k=1./smoothness) for pi in p])
 
 # mean squared error for the target parameter
-def mean_squared_error(y,yo):
+def mean_squared_error(y, yo):
     return sum([(yi-yio)**2 for yi,yio in zip(y,yo)])
 
 # Minimax penalty using the approximate step function
-def minmax_penalty(vec):
-    return sum(approximate_step_function(x) for x in vec)
+def minmax_penalty(vec, smoothness=1):
+    return sum(approximate_step_function(x, smoothness) for x in vec)
 
